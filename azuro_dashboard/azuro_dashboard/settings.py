7ca69973d04d0e3a15335cc4941e74c35cd77a38
arsi44 <gjenferd23@gmail.com>
2023-11-23 17:18:38 +0300

    fix

--- azuro_dashboard/azuro_dashboard/settings.py
+++ azuro_dashboard/azuro_dashboard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "the key here!"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['azuro.pythonanywhere.com', ]
 
 
 # Application definition
